2deb19ec7e28759c74c29358825ec5afd431d42b
Alfredo de la Calle <alfredo.delacalle.contractor@bbva.com>
2018-05-07 08:00:38 +0200

    [del] residual files

--- client/src/components/Indicators/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export { default as IndicatorDirection } from './Direction';
-export { default as IndicatorRpm } from './Rpm';
-export { default as IndicatorSpeedometer } from './Speedometer';
-export { default as IndicatorGear } from './Gear';
--- client/src/components/index.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-export { default as Header } from './Header';
-export { default as Car } from './Car';
-
